758867a3fbb0abf55cb8268fa8cd7b459107fb18
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-27 14:09:03 -0400

    added fly method

--- jets.js
+++ jets.js
@@ -1,6 +1,10 @@
-var Jet = function (color, size) {
+let Jet = function(color, size) {
   this.color = color;
   this.size = size;
-}
+
+  this.fly = function() {
+    console.log('whoooooosh!');
+  };
+};
 
 module.exports = Jet;
